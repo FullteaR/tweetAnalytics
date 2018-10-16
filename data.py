8188f8b525e7e1981b8cfbd1bf7ceee69d63c79b
Unknown <rikuta0811@g.ecc.u-tokyo.ac.jp>
2018-10-16 14:00:02 +0900

    fix: use scikit
    and so on...

--- data.py
+++ data.py
@@ -1,1 +1,3 @@
-accounts=[]
+accounts=["bayern1049","kuni_sawa","katcccchan","ari0407ari","Yasaka_8hk","haruya_s32","longer_u","ss_aeaea","motohar61987696","h8i__","kim1014_41","utokyoshinkawa","sota_155","___fuvm","ryoya34599080","solt_sato","jukain_178","seeker__Taka","hayao_sayuden","ce4EHOLK8UcC1RH","kumarcturus","yhis_ush","MinorinPart3","QGRWuB1zcxoTgJh","aTwUJJhX9SSsZni"]
+
+bot="bot_41th"
